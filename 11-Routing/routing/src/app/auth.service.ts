64634d7c1bb4814291cc88a1adeb8869f19029f8
smileisak <ikaboubi@gmail.com>
2018-08-24 16:28:56 +0200

    Protecting routes with canActivate

---
+++ 11-Routing/routing/src/app/auth.service.ts
@@ -0,0 +1,24 @@
+
+export class AuthService {
+  private loggedIn = false;
+
+  isAuthenticated() {
+    const promise = new Promise(
+      (resolve, reject) => {
+        setTimeout(() => {
+          resolve(this.loggedIn);
+        }, 800);
+
+        // resolve(this.loggedIn);
+      }
+    );
+    return promise;
+  }
+  login() {
+    this.loggedIn = true;
+  }
+
+  logout() {
+    this.loggedIn = false;
+  }
+}
