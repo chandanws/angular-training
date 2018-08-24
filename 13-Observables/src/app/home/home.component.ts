f711dc35fa6693660bb39b2b0f3eb3c4d2ed2f2a
smileisak <ikaboubi@gmail.com>
2018-08-24 21:07:11 +0200

    Build and use a custom observable from scratch

--- 13-Observables/src/app/home/home.component.ts
+++ 13-Observables/src/app/home/home.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 // tslint:disable-next-line:import-blacklist
 import 'rxjs/Rx';
+import { Observer } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-home',
@@ -13,10 +14,44 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    const myNumbers = Observable.interval(1000);
-    myNumbers.subscribe(
-      (number: number) => {
-        console.log(number);
+    // const myNumbers = Observable.interval(1000);
+    // myNumbers.subscribe(
+    //   (number: number) => {
+    //     console.log(number);
+    //   }
+    // );
+
+    const myObservable = Observable.create(
+      (observer: Observer<string>) => {
+        setTimeout(() => {
+          observer.next('first package');
+        }, 2000);
+
+        setTimeout(() => {
+          observer.next('second package');
+        }, 4000);
+
+        setTimeout(() => {
+          // observer.error('This does not work ');
+          observer.complete();
+        }, 5000);
+
+        setTimeout(() => {
+          // observer.error('This does not work ');
+          observer.next('third message');
+        }, 6000);
+      }
+    );
+
+    myObservable.subscribe(
+      (message: string) => {
+        console.log('The Message Is: ' + message);
+      },
+      (error: string) => {
+        console.log('The Error Is: ' + error );
+      },
+      () => {
+        console.log('Completed');
       }
     );
   }
