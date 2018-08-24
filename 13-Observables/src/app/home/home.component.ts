8e771f3f4326ae6959e5896f33a7d8f3792f87b9
smileisak <ikaboubi@gmail.com>
2018-08-24 20:54:51 +0200

    Start Observable: Build and use a First Simple Observable

---
+++ 13-Observables/src/app/home/home.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+// tslint:disable-next-line:import-blacklist
+import 'rxjs/Rx';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    const myNumbers = Observable.interval(1000);
+    myNumbers.subscribe(
+      (number: number) => {
+        console.log(number);
+      }
+    );
+  }
+
+}
